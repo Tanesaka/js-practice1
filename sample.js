01f6c3f2c1e6c79752f114ff929f2c495fdba929
Tanesaka <seedtanesa@outlook.jp>
2020-12-10 00:33:18 +0900

    add

--- sample.js
+++ sample.js
@@ -1,26 +1,7 @@
-let youtuber = {
-  list: {
-    business: {
-      youtuber: [
-        { name: "しまぶー", age: 20, teachPrograming() {} },
-        { name: "あっちゃん", age: 37, teachHistory() {} }
-      ],
-      teach() {}
-    },
-    entertainment: {
-      youtuber: [{ name: "ヒカキン" }, { name: "はじめしゃちょー"}],
-      makesmile() {}
-    }
-  },
-  plan() {},
-  uploadVideo() {}
-};
+async function callApi (){
+  const $res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await $res.json();
+  console.log(users);
+}
 
-const $nextBtn = document.getElementById("nextBtn");
-const $nextBtnWidth = $nextBtn.offsetWidth;
-const $nextBtnHeight = $nextBtn.offsetHeight;
-const $nextBtnArea = $nextBtnWidth * $nextBtnHeight;
-
-const $foo = 100;
-
-let $bar = 200;
+callApi();
